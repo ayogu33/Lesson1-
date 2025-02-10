731d0d33d62fa6433bf28b7f01464272bf8170fa
ayogu33 <ayoguchibuezekingsley1@gmail.com>
2025-02-10 10:14:48 -0800

    personal  class

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
-let x = 16 + "Volvo";
-console.log(x)
+let x = 16 + " Volvo";
+x = "YOU";
+let y = "Volvo " + 16
+let z = 45 + 45 + " Volvo"
+let a = 4 + 6
+console.log(x, y, z, a)
+
+const s = 5
+console.log(s)
+
+let answer1 = " I'm okay!"
+let answer2 = 'I am okay "alrigt!"'
+console.log(answer1, answer2)
+
+let cars = ["Totota", "Lexus", "BMW", "Camery"]
+console.log(cars)
+
+const names = {fullname1:"Quinn Talen", fullname2:"Sill Blade", fullname3:"Peter Chulk", fullname4:"Vorden Blade"}
+const person = { firstname:"Layla", lastname:"maurow", age:1556, eyecolor:"brown", haircolor:"pink"}
+console.log(names, person)
+
+const name = {name1:"Erin Helly", nme2:"Leo Suiyan"}
+console.log(name)
+
+function myfunction(p1, p2){
+    return (p1 * p2)
+}
+let result = myfunction(6, 4)
+console.log(result)
